29dfd79d5550844e8a117293fc0e235db8dc0781
haneen work <bloomandcuddle@gmail.com>
2025-11-21 12:03:44 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
+// إدخال الهيدر
+fetch('/includes/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('header').innerHTML = data;
+    });
+
+// إدخال الفوتر
+fetch('/includes/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('footer').innerHTML = data;
+    });
+
+// فتح/إغلاق القائمة على الهاتف
+document.addEventListener('click', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+
+    if (menuToggle) {
+        menuToggle.addEventListener('click', () => {
+            nav.classList.toggle('open');
+        });
+    }
+});
 
